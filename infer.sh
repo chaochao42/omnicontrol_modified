e44282706cd74f8bc252e15c19cd5c65ef852f81
chaochao42 <2504483651@qq.com>
2024-01-28 12:53:15 +0800

    video 1 setting

--- infer.sh
+++ infer.sh
@@ -2,7 +2,7 @@ python -m sample.generate \
 --model_path ./save/omnicontrol_ckpt/model_humanml3d.pt \
 --num_repetitions 1 \
 --text_prompt predefined \
---output_dir output/new \
+--output_dir output/video2 \
 --cond_mode only_spatial \
 --seed 12 \
 --motion_length 196
@@ -18,8 +18,8 @@ python inference.py \
 --start_position 2.9 0.9 -1.8 \
 --camera_angle -60 0 0 \
 --camera_position 3 6.0 2.0 \
---path /home/sjtu/Desktop/yuchaowang/OmniControl/output/new \
---video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video \
+--path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video2 \
+--video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video2 \
 --glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/bedroom_0128.glb
 
 cd /home/sjtu/Desktop/yuchaowang/OmniControl
