64b6776c212d57f28444ba4253e04ef8f11a5a41
chaochao42 <2504483651@qq.com>
2024-01-28 18:49:00 +0800

    video 4 setting

--- infer.sh
+++ infer.sh
@@ -2,10 +2,10 @@ python -m sample.generate \
 --model_path ./save/omnicontrol_ckpt/model_humanml3d.pt \
 --num_repetitions 1 \
 --text_prompt predefined \
---output_dir output/video3 \
+--output_dir output/video4 \
 --cond_mode only_spatial \
 --seed 5 \
---motion_length 140
+--motion_length 196
 
 cd /home/sjtu/Desktop/yuchaowang/SemanticBoost
 
@@ -15,11 +15,11 @@ python inference.py \
 --size 2048 \
 --render "3dfast" \
 --role "Bill Gates" \
---start_position 2.0 0.9 -1.7 \
+--start_position 2.0 0.9 -1.0 \
 --camera_angle -60 0 0 \
---camera_position 3 6.0 1.0 \
---path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video3 \
---video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video3 \
---glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/bedroom_0128_v3.glb
+--camera_position 3 6.0 0.0 \
+--path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video4 \
+--video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video4 \
+--glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/tv_room_0128_v4.glb
 
 cd /home/sjtu/Desktop/yuchaowang/OmniControl
--- render.sh
+++ render.sh
@@ -6,11 +6,11 @@ python inference.py \
 --size 3200 \
 --render "3dfast" \
 --role "Bill Gates" \
---start_position 2.1 0.9 -1.9 \
+--start_position 1.8 0.9 -0.92 \
 --camera_angle -60 0 0 \
 --camera_position 3 6.0 0.0 \
---path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video3 \
---video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video3 \
---glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/bedroom_0128_v3.glb
+--path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video4 \
+--video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video4 \
+--glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/tv_room_0128_v4.glb
 
 cd /home/sjtu/Desktop/yuchaowang/OmniControl
