93f4dd3a8d5c5dfadb3c401f180a048796ce5ced
chaochao42 <2504483651@qq.com>
2024-01-29 22:52:18 +0800

    video 10 setting

--- infer.sh
+++ infer.sh
@@ -2,9 +2,9 @@ python -m sample.generate \
 --model_path ./save/omnicontrol_ckpt/model_humanml3d.pt \
 --num_repetitions 1 \
 --text_prompt predefined \
---output_dir output/video9 \
+--output_dir output/video10 \
 --cond_mode only_spatial \
---seed 8 \
+--seed 9 \
 --motion_length 196
 
 cd /home/sjtu/Desktop/yuchaowang/SemanticBoost
@@ -15,11 +15,11 @@ python inference.py \
 --size 3200 \
 --render "3dfast" \
 --role "Bill Gates" \
---start_position 3.0 0.9 -0.55 \
---camera_angle -50 0 0 \
---camera_position 4 5.5 2.0 \
---path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video9 \
---video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video9 \
---glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/dining_room_0128_v9.glb
+--start_position 3.0 0.9 -0.9 \
+--camera_angle -30 0 0 \
+--camera_position 3.0 4.5 2.0 \
+--path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video10 \
+--video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video10 \
+--glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/lunch_room_0128_v10.glb
 
 cd /home/sjtu/Desktop/yuchaowang/OmniControl
--- render.sh
+++ render.sh
@@ -6,11 +6,11 @@ python inference.py \
 --size 3200 \
 --render "3dfast" \
 --role "Bill Gates" \
---start_position 3.2 0.9 -1.1 \
---camera_angle -50 0 0 \
---camera_position 4 5.5 2.0 \
---path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video9 \
---video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video9 \
---glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/dining_room_0128_v9.glb
+--start_position 3.0 0.9 -0.9 \
+--camera_angle -30 0 0 \
+--camera_position 3.0 4.5 2.0 \
+--path /home/sjtu/Desktop/yuchaowang/OmniControl/output/video10 \
+--video_path /home/sjtu/Desktop/yuchaowang/OmniControl/llm_long/video10 \
+--glb_file_path /home/sjtu/Desktop/yuchaowang/SemanticBoost/glb_files/lunch_room_0128_v10.glb
 
 cd /home/sjtu/Desktop/yuchaowang/OmniControl
